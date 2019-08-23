adafb6f72ba862676d23c1d9bc0049a9203a5c1a
Nicolas <nyko_1600@hotmail.com>
2019-08-23 19:07:29 -0300

    fin desa - no test

---
+++ src/main/java/com/biometricservice/library/controller/GetUser.java
@@ -0,0 +1,26 @@
+package com.biometricservice.library.controller;
+
+import com.biometricservice.library.service.RestApiCall;
+import org.json.JSONObject;
+
+public class GetUser {
+
+  public boolean userHasConfirmed(String userId) throws Exception {
+    String uri = buildUri(userId);
+    JSONObject resp = null;
+    try {
+      resp = RestApiCall.INSTANCE.getMethod(uri);
+    } catch (Exception e) {
+      throw e;
+    }
+
+    if (resp !=null && resp.getString("message").equals("user exist")) {
+      return true;
+    }
+    return false;
+  }
+
+  private String buildUri(String userId) {
+    return new String("https://test4.ddns.net:8443/fingerService-api/search?user_id"+userId);
+  }
+}
---
+++ src/main/java/com/biometricservice/library/service/RestApiCall.java
@@ -0,0 +1,40 @@
+package com.biometricservice.library.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+
+import static java.lang.Thread.sleep;
+
+public enum RestApiCall {
+  INSTANCE;
+  public JSONObject getMethod(String url){
+    try {
+      OkHttpClient client = new OkHttpClient();
+      Response response=null;
+
+      int count  =10;
+      while (count>0){
+        Request request = new Request.Builder()
+            .get()
+            .url(url)
+            .addHeader("Accept", "application/json")
+            .build();
+
+        response = client.newCall(request).execute();
+        if (response.code() == 200) {
+          String resp = response.body().string();
+          return new JSONObject(resp);
+        }
+        sleep(500);
+        count--;
+      }
+      throw new RuntimeException(url + ": Failed HTTP error code " + response.code());
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+}
