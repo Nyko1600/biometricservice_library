15b0ca5aef8d07862fbb78a0ed6b617c000af331
Nicolas <nyko_1600@hotmail.com>
2019-11-06 21:46:14 -0300

    crear clase main

---
+++ src/main/java/com/biometricservice/library/Main.java
@@ -0,0 +1,10 @@
+package com.biometricservice.library;
+
+import com.biometricservice.library.controller.GetUser;
+
+public class Main {
+  public static void main(String[] args) {
+    GetUser getUser = new GetUser();
+
+  }
+}
